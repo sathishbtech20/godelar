45c905f1fed3b8e3067de3d31fca967cedad57c7
Sathishkumar <sathishbtech20@gmail.com>
2019-01-31 11:05:19 +0530

    Home class created

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+class Home
+{
+	public function __construct()
+	{
+	}
+	
+	public function getPageTitle()
+	{
+		return 'Home page';
+	}
+}
+
+$home = new Home();
+
+echo $home->getPageTitle();
